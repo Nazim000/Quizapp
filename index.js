999f72379b962129eb4ec501c08504ab6dc8793b
Nazim <mna329474@gmail.com>
2023-12-27 22:52:31 +0500

    quizapp intial

---
+++ index.js
@@ -0,0 +1,66 @@
+export const data = [
+    {
+        question: 'What does HTML stand for?',
+        option1: 'Hyper Tag Markup Language',
+        option2: 'Hyper Text Markup Language',
+        option3: 'Hyperlinks Text Mark Language',
+        option4: 'Hyperlinking Text Marking Language',
+        ans: 2
+    },
+    {
+        question: '_________ keyword is used to declare variables in javascript.',
+        option1: 'Var',
+        option2: 'Dim',
+        option3: 'String',
+        option4: 'None',
+        ans: 1
+    },
+    {
+        question: 'Which of these is a genuine tag keyword?',
+        option1: 'Header',
+        option2: 'Bold',
+        option3: 'Body',
+        option4: 'Image',
+        ans: 3
+    },
+    {
+        question: 'What does CSS stand for?',
+        option1: 'Computing Style Sheet',
+        option2: 'Creative Style System',
+        option3: 'Cascading Style Sheet',
+        option4: 'Creative Styling Sheet',
+        ans: 3
+    },
+    {
+        question: 'Which of the following options is correct with regard to HTML?',
+        option1: ' It is a modelling language',
+        option2: ' It is a scripting language',
+        option3: 'It is a partial programming language',
+        option4: 'It is used to structure documents',
+        ans: 4
+    },
+    {
+        question: ' When a JavaScript object is sent to Java, the runtime engine creates a Java wrapper of type',
+        option1: 'ScriptObject',
+        option2: 'JSObject',
+        option3: 'JavaObject',
+        option4: 'Jobject',
+        ans: 2
+    },
+    {
+        question: '____________ is the tainted property of a window object.',
+        option1: 'Pathname',
+        option2: 'Protocol',
+        option3: 'Defaultstatus',
+        option4: 'Host',
+        ans: 3
+    },
+    {
+        question: ' In JavaScript, _________ is an object of the target language data type that encloses an object of the source language.',
+        option1: 'a wrapper',
+        option2: 'a link',
+        option3: 'a cursor',
+        option4: ' a form',
+        ans: 1
+    }
+];
